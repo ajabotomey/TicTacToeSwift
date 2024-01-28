fa8028d16e8c409a902655468894f7136f4bcc4d
ajabotomey <killerbyte_93@hotmail.com>
2024-01-28 20:13:05 +1000

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Andrew Abotomey on 28/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
